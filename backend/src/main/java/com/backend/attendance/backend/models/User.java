4c4b976602ab2fcf124173344d802f5328dc8bec
Jenish Togadiya <jenishtogadiya549@gmail.com>
2025-02-24 18:20:23 +0530

    create-models

---
+++ backend/src/main/java/com/backend/attendance/backend/models/User.java
@@ -0,0 +1,48 @@
+package com.backend.attendance.backend.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String email;
+    private String password;
+    private String rollNumber;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRollNumbeer() {
+        return rollNumber;
+    }
+
+    public void setRollNumbeer(String rollNumbeer) {
+        this.rollNumber = rollNumbeer;
+    }
+}
