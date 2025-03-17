3f1ca2aa6827eb3801ed4e0788a9a3d02a9d4a34
Divii2205 <divijaa.24bcs10206@sst.scaler.com>
2025-03-17 21:47:13 +0530

    Improve Functionality of Mark Attendance Page

--- Attendance-Automation-Android/app/src/main/java/com/android/attendance_automation_android/MainActivity.java
+++ Attendance-Automation-Android/app/src/main/java/com/android/attendance_automation_android/MainActivity.java
@@ -2,11 +2,25 @@ package com.android.attendance_automation_android;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+//    TextView signInMessage = findViewById(R.id.signInMessage);
+    public void signIn(View view){
+        EditText sstMail = (EditText) findViewById(R.id.sstMail);
+        EditText sstRollNo = (EditText) findViewById(R.id.sstRollNo);
+
+        Log.i("SignInButton", "Sign In Attempted");
+        Log.i("Mail", sstMail.getText().toString());
+        Log.i("RollNo", sstRollNo.getText().toString());
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- Attendance-Automation-Android/app/src/main/java/com/android/attendance_automation_android/MarkAttendance.java
+++ Attendance-Automation-Android/app/src/main/java/com/android/attendance_automation_android/MarkAttendance.java
@@ -1,9 +1,25 @@
 package com.android.attendance_automation_android;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MarkAttendance extends AppCompatActivity {
+    public void markAttendance(View view){
+        ImageView attendanceStatus = (ImageView) findViewById(R.id.alert);
+        attendanceStatus.setImageResource(R.drawable.baseline_check_circle_24);
+
+        TextView attendanceStatusText = (TextView) findViewById(R.id.attendance_status);
+        attendanceStatusText.setText("DSA Attendance Marked!");
+
+        TextView attendanceButton = (TextView) findViewById(R.id.attendanceButton);
+        attendanceButton.setVisibility(View.GONE);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
