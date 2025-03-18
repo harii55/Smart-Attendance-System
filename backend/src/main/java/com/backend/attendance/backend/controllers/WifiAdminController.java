f01d3ec6f19985708c7696c58ecdcd5653c251c0
harii55 <hariii5389@gmail.com>
2025-03-18 16:07:50 +0530

    prog: Created Admin Service for Wifi Monitoring

--- backend/src/main/java/com/backend/attendance/backend/controllers/WifiAdminController.java
+++ backend/src/main/java/com/backend/attendance/backend/controllers/WifiAdminController.java
@@ -4,24 +4,47 @@ import com.backend.attendance.backend.models.WifiAdminStartRequest;
 import com.backend.attendance.backend.models.WifiAdminStartResponse;
 import com.backend.attendance.backend.models.WifiStudentRequest;
 import com.backend.attendance.backend.models.WifiStudentResponse;
+import com.backend.attendance.backend.services.WifiAdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 @RestController
 @RequestMapping("/attendance/wifi/admin")
 public class WifiAdminController {
 
-    @PostMapping("/start")
-    public ResponseEntity<?> startMonitoring(@RequestBody WifiAdminStartRequest request) throws Exception {
-        if (request.getMonitoring()) {
-            return ResponseEntity.ok(new WifiAdminStartResponse("true"));
-        }else{
-            return ResponseEntity.ok(new WifiAdminStartResponse("false"));
-        }
+//    @PostMapping("/start")
+//    public ResponseEntity<?> startMonitoring(@RequestBody WifiAdminStartRequest request) throws Exception {
+//        if (request.getMonitoring()) {
+//            return ResponseEntity.ok(new WifiAdminStartResponse("true"));
+//        }else{
+//            return ResponseEntity.ok(new WifiAdminStartResponse("false"));
+//        }
+//
+//    }
 
+    @Autowired
+    private final WifiAdminService wifiAdminService;
+
+    public WifiAdminController(WifiAdminService wifiAdminService) {
+        this.wifiAdminService = wifiAdminService;
+    }
+
+    @PostMapping("/start")
+    ResponseEntity<?> startMonitoring(@RequestBody WifiAdminStartRequest request) throws Exception {
+       if(request.getMonitoring()){
+
+           return ResponseEntity.ok(wifiAdminService.startMonitoring(request));
+       }else {
+
+           return ResponseEntity.ok(wifiAdminService.startMonitoring(request));
+       }
     }
 
 }
---
+++ backend/src/main/java/com/backend/attendance/backend/services/WifiAdminService.java
@@ -0,0 +1,47 @@
+package com.backend.attendance.backend.services;
+
+import com.backend.attendance.backend.models.WifiAdminStartRequest;
+import com.backend.attendance.backend.models.WifiAdminStartResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@Service
+public class WifiAdminService {
+    private HashMap<String, HashMap<String, ArrayList<String>>> monitoringStatusMap = new HashMap<>();
+
+
+    public ResponseEntity<?> startMonitoring(@RequestBody WifiAdminStartRequest request) throws Exception {
+        String year = request.getYear();
+        String batch = request.getBatch();
+        String subject = request.getSubject();
+        String status = request.getMonitoring().toString();
+
+        if (request.getMonitoring()) {
+            System.out.println("Starting Monitoring");
+
+
+            HashMap<String, ArrayList<String>> monitoringStatusInnerMap = new HashMap<>();
+            ArrayList<String> monitoringStatusList = new ArrayList<>();
+
+            monitoringStatusList.add(0,subject);
+            monitoringStatusList.add(1,status);
+
+            monitoringStatusInnerMap.put(batch,monitoringStatusList);
+            monitoringStatusMap.put(year,monitoringStatusInnerMap);
+
+
+            return ResponseEntity.ok(new WifiAdminStartResponse("true"));
+        }else{
+            System.out.println("Stop Monitoring");
+            monitoringStatusMap.get(year).remove(batch);
+
+            return ResponseEntity.ok(new WifiAdminStartResponse("false"));
+        }
+    }
+
+}
