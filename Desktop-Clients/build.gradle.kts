37e55af6fe9044ee17fc3d2f38a5fdd5c14b18a2
Jenu <165951149+Jenish-1235@users.noreply.github.com>
2025-04-30 20:48:58 +0530

    Update build.gradle.kts

--- Desktop-Clients/build.gradle.kts
+++ Desktop-Clients/build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 plugins {
     kotlin("jvm")
     id("org.jetbrains.compose")
-    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 group = "com.attendance"
