5b20253255b7412828cb57a3f7c299a5548911f2
Jenish-1235 <jenishtogadiya549@gmail.com>
2025-05-07 16:38:25 +0530

    UI : Implemented Client side UI Updates for Attendance Sessions.

--- Desktop-Clients/src/main/kotlin/network/WebSocketClient.kt
+++ Desktop-Clients/src/main/kotlin/network/WebSocketClient.kt
@@ -9,27 +9,58 @@ import io.ktor.websocket.*
 import kotlinx.coroutines.*
 import java.time.Instant
 
-class WebSocketClient(private val email: String, private val bssid: String) {
-    private val client = HttpClient(CIO) {
-        install(WebSockets)
-    }
-
+class WebSocketClient(
+    private val endpoint: String,
+    private val email: String,
+    private val bssid: String,
+    private val onStatusChange: (String) -> Unit = {},
+    private val onConnectionUpdate: (Int) -> Unit = {},
+    private val onError: (String) -> Unit = {}
+) {
+    private val client = HttpClient(CIO) { install(WebSockets) }
     suspend fun connect() {
         try {
-            println("Connected to bssid: $bssid")
-            client.webSocket(
-                urlString = "ws://4.213.5.84:8000/attendance/wifi/ws"
-            ) {
+            client.webSocket(urlString = endpoint) {
+                onStatusChange("Connected to attendance session. Chill now...")
+                val basePayload = """{"email":"$email","bssid":"$bssid"}""";
+                send(Frame.Text(basePayload))
+                var startTime = System.currentTimeMillis()
 
-                println("Connected to bssid: $bssid")
-                println("email : $email")
-                while (true) {
-                    send(Frame.Text("""{"email": "$email", "bssid": "$bssid"}"""))
-                    delay(5000)
+                val timer = CoroutineScope(Dispatchers.IO).launch {
+                    while (true) {
+                        delay(1000)
+                        val elapsed = (System.currentTimeMillis() - startTime) / 1000
+                        onConnectionUpdate(elapsed.toInt())
+                    }
+                }
+
+                val heartbeatJob = launch (Dispatchers.IO) {
+                    while (isActive) {
+                        delay(5000)
+                        send(Frame.Text(basePayload))
+                    }
+                }
+
+                try {
+                    for (frame in incoming) {
+                        if (frame is Frame.Text) {
+                            println("➔ ${frame.readText()}")
+                            onStatusChange(frame.readText())
+                        }
+                    }
+                } finally {
+                    heartbeatJob.cancelAndJoin()
+                    timer.cancelAndJoin()
                 }
             }
+        } catch (e: CancellationException) {
+            onError("Connection Cancelled")
         } catch (e: Exception) {
-            println("❌ WebSocket Error: ${e.message}")
+            onError("Connection Error: ${e.message}")
+        }finally {
+            client.close()
         }
     }
 }
+
+
